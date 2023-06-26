20462e93e0e2d14a924cb46f28b2920299da56b6
Himasai Reddy <bhsr2003@gmail.com>
2023-06-26 13:39:14 +0530

    Learing modulization

---
+++ helper.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"strings"
+)
+
+func validateUserInput(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
+	isValidName := len(firstName) >= 2 && len(lastName) >= 2
+	isValidEmail := strings.Contains(email, "@")
+	isVaildTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+
+	return isValidName, isValidEmail, isVaildTicketNumber
+}
--- main.go
+++ main.go
@@ -66,14 +66,6 @@ func getFirstNames() []string {
 	return fNames
 }
 
-func validateUserInput(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
-	isValidName := len(firstName) >= 2 && len(lastName) >= 2
-	isValidEmail := strings.Contains(email, "@")
-	isVaildTicketNumber := userTickets > 0 && userTickets <= remainingTickets
-
-	return isValidName, isValidEmail, isVaildTicketNumber
-}
-
 func getUserInput() (string, string, string, uint) {
 	var firstName string
 	var lastName string
